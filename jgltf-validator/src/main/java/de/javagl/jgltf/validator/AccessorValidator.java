829d8c53af38dc0b41c39431bda46a2b88abdc28
Marco Hutter <javagl@javagl.de>
2017-01-13 19:11:51 +0100

    Added accessor alignment validation

--- jgltf-validator/src/main/java/de/javagl/jgltf/validator/AccessorValidator.java
+++ jgltf-validator/src/main/java/de/javagl/jgltf/validator/AccessorValidator.java
@@ -27,7 +27,10 @@
 package de.javagl.jgltf.validator;
 
 import de.javagl.jgltf.impl.Accessor;
+import de.javagl.jgltf.impl.BufferView;
 import de.javagl.jgltf.impl.GlTF;
+import de.javagl.jgltf.model.Accessors;
+import de.javagl.jgltf.model.GltfConstants;
 
 /**
  * A class for validating {@link Accessor} objects
@@ -77,6 +80,80 @@ class AccessorValidator extends AbstractGltfValidator
         if (validatorResult.hasErrors())
         {
             return validatorResult;
+        }
+        
+        // Validate the alignment constraints of the accessor, for the
+        // components (e.g. float), and the elements (e.g. 3D float vectors),
+        // referring to the byte offset (in its buffer view), and the total 
+        // byte offset (in the buffer)
+        
+        Integer componentType = accessor.getComponentType();
+        if (componentType == null)
+        {
+            validatorResult.addError("The componentType is null", context);
+            return validatorResult;
+        }
+            
+        String componentTypeName = 
+            GltfConstants.stringFor(accessor.getComponentType());
+        int componentSizeInBytes =
+            Accessors.getNumBytesForAccessorComponentType(componentType);
+        int numComponents = 
+            Accessors.getNumComponentsForAccessorType(accessor.getType());
+        int elementSizeInBytes = numComponents * componentSizeInBytes;
+
+        BufferView bufferView = getGltf().getBufferViews().get(bufferViewId);
+        int accessorByteOffset = accessor.getByteOffset();
+        int totalByteOffset = bufferView.getByteOffset() + accessorByteOffset;
+
+        if (accessorByteOffset % componentSizeInBytes != 0)
+        {
+            validatorResult.addError(
+                "The byteOffset is " + accessorByteOffset + ", but must be " + 
+                "aligned to the size of the component type (" + 
+                componentTypeName + "), which is "+componentSizeInBytes, 
+                context);
+            return validatorResult;
+        }
+        if (totalByteOffset % componentSizeInBytes != 0)
+        {
+            validatorResult.addError(
+                "The total byte offset from the beginning of buffer " + 
+                bufferView.getBuffer() + " is " + totalByteOffset + 
+                ", but must be aligned to the size of the component type (" + 
+                componentTypeName + "), which is "+componentSizeInBytes, 
+                context);
+            return validatorResult;
+        }
+
+        if (!isVersionGreaterThanOrEqual("1.1"))
+        {
+            // In glTF 1.0, the accessors had to be aligned to the size of 
+            // the FULL type (called "elements" here). But nobody obeyed 
+            // this constraint. Issue only a warning in this case.
+            
+            if (accessorByteOffset % elementSizeInBytes != 0)
+            {
+                validatorResult.addWarning(
+                    "The byteOffset is " + accessorByteOffset + 
+                    ", but must be aligned to the size of the elements (" + 
+                    accessor.getType() + " with " + componentTypeName + "), " + 
+                    "which is "+elementSizeInBytes, 
+                    context);
+                return validatorResult;
+            }
+            if (totalByteOffset % elementSizeInBytes != 0)
+            {
+                validatorResult.addWarning(
+                    "The total byte offset from the beginning of buffer " + 
+                    bufferView.getBuffer() + " is " + totalByteOffset + 
+                    ", but must be aligned to the size of the elements (" + 
+                    accessor.getType() + " with " + componentTypeName + "), " + 
+                    "which is "+elementSizeInBytes, 
+                    context);
+                return validatorResult;
+            }
+            
         }
         
         return validatorResult;
